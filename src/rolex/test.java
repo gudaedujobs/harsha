59dc0cef5af9fc8550f29cebda327a44ad78cf81
rathodrolex <sinhavikram728@gmail.com>
2023-05-06 11:43:36 +0530

    branchx

--- src/rolex/test.java
+++ src/rolex/test.java
@@ -6,7 +6,7 @@ public class test {
 	{
 	    public static void main(String []args)
 	    {
-	        System.out.println("My First cloning from github.");
+	        System.out.println("My First cloning from githubs.");
 	    }
 	}
 
